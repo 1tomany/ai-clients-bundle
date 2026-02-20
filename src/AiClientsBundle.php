60627d39c2b5bf0cb0a74e4b39df1b056626da00
Vic Cherubini <vcherubini@gmail.com>
2026-02-20 16:42:52 -0600

    wip

--- src/AiClientsBundle.php
+++ src/AiClientsBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OneToMany\AI\Clients\Bundle;
+namespace OneToMany\Bundle;
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class Bundle extends AbstractBundle
+class AiClientsBundle extends AbstractBundle
 {
     /**
      * @var non-empty-list<'file'|'query'>
